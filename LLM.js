5a50f25b518b8762c404a1f7982231d573cc0f28
Amarjeet <amarjeet.btechcse@gmail.com>
2025-06-28 12:42:16 +0530

    day 2 completed

--- LLM.js
+++ LLM.js
@@ -1,13 +1,39 @@
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI } from "@google/genai";//INSTALL THIS ON TERMINAL
+import readlineSync from 'readline-sync';//INSTALL THIS ON TERMINAL
 
 const ai = new GoogleGenAI({ apiKey: "AIzaSyCqherhMYqN7uQUNku-WhWmnr5dyv3qmW0" });
 
-async function main() {
+const History = []
+
+async function Chatting(userProblem) {
+
+  History.push({
+    role:'user',
+    parts:[{text:userProblem}]
+  })
+
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash",
-    contents: "Explain Array in details.",
+    contents: History
   });
+  
+
+  History.push({
+    role:'model',
+    parts:[{text:response.text}]
+  })
+  
+  console.log("\n");
   console.log(response.text);
 }
 
-await main();
+
+async function main(){
+   
+   const userProblem = readlineSync.question("Ask me anything--> ");
+   await Chatting(userProblem);
+   main();
+}
+
+
+main();
